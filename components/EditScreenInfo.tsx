53cca7b24c718d9e822d6f6c00118658041c180d
Hussariush <aethelbrihte@gmail.com>
2021-01-17 13:59:15 +0100

    Initial screen setup

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,53 +1,42 @@
 import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TouchableOpacity, TextInput, Button, Alert } from 'react-native';
 
 import Colors from '../constants/Colors';
 import { MonoText } from './StyledText';
 import { Text, View } from './Themed';
 
-export default function EditScreenInfo({ path }: { path: string }) {
+const EditScreenInfo = ({ path }: { path: string }) => {
+  const [value, onChangeValue] = useState<string>('0')
   return (
-    <View>
-      <View style={styles.getStartedContainer}>
+    <View>      
         <Text
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
-          Open up the code for this screen:
+          Enter your amount
         </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-          darkColor="rgba(255,255,255,0.05)"
-          lightColor="rgba(0,0,0,0.05)">
-          <MonoText>{path}</MonoText>
-        </View>
-
         <Text
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
-          Change any of the text, save the file, and your app will automatically update.
+          {value}
         </Text>
-      </View>
-
-      <View style={styles.helpContainer}>
-        <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-          <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making changes
-          </Text>
-        </TouchableOpacity>
-      </View>
+        <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 200 }}
+        keyboardType={'numeric'}
+        onChangeText={amount => onChangeValue(amount)}
+        value={value}
+       />
+       <Button
+        title="Calculate"
+        color="#f194ff"
+        onPress={() => Alert.alert('Button with adjusted color pressed')}
+      />
     </View>
   );
 }
 
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet'
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -78,6 +67,7 @@ const styles = StyleSheet.create({
   getStartedContainer: {
     alignItems: 'center',
     marginHorizontal: 50,
+    flexDirection: 'column',
   },
   homeScreenFilename: {
     marginVertical: 7,
@@ -106,3 +96,5 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
 });
+
+export default EditScreenInfo;
