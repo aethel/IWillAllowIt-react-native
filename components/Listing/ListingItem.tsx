6a46fe2f56ff11bea3fa73d56437ebb6e10637f2
Hussariush <aethelbrihte@gmail.com>
2021-01-30 11:24:49 +0100

    Deductions handler

--- components/Listing/ListingItem.tsx
+++ components/Listing/ListingItem.tsx
@@ -1,12 +1,17 @@
-import React from "react";
-import { View, Text, Button, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, TouchableOpacity, TextInput } from "react-native";
 import NotFoundScreen from "../../screens/NotFoundScreen";
 import { DayAllowance } from "./Listing";
 
 const ListingItem = ({item, onPressHandler, index}:{item:DayAllowance, onPressHandler: (val:any, index:any) => void, index:any}) => {
-   
+    const [deductionValue, setDeductionValue] = useState<string>('0');
+    const [additionValue, setAdditionValue] = useState<string>('0');
+    
+    const onDeductValue = (value:string) => setDeductionValue(value);
+    const onAddValue = (value:string) => setAdditionValue(value);
+    
     const clickHandle = () => {
-        onPressHandler(1, index)
+        onPressHandler(deductionValue, index)
     }
    
     return (
@@ -24,6 +29,14 @@ const ListingItem = ({item, onPressHandler, index}:{item:DayAllowance, onPressHa
             {!!item.additions.length && <Text>
                 deductions: {item.additions}
             </Text>}
+            <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 200, backgroundColor:'#f0f0f0' }}
+                keyboardType={'numeric'}
+                onChangeText={amount => onDeductValue(amount)}
+                value={deductionValue}
+                placeholder='Deduct this much'
+                placeholderTextColor='#000'
+            />
             <Button title={'deduct'} onPress={clickHandle}/>
         </View>
     </TouchableOpacity>
